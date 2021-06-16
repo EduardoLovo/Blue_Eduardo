461924671da30b9dd027bf0e7535c81a538692f5
EduardoLovo <eduardo.llovo@gmail.com>
2021-06-15 22:27:50 -0300

    ex

--- Aula7/Exercicio04.py
+++ Aula7/Exercicio04.py
@@ -1,3 +1,11 @@
-print('Ola mundo')
+# 04 - Desenvolva um código em que o usuário vai entrar vários números e no final vai apresentar a
+# soma deles (o usuário vai dizer quantos números serão informados antes de começar)
 
-print('teste commit')
+soma = 0
+
+quantidade_Numeros = int(input('Quantidade de somas: '))
+
+for i in range(quantidade_Numeros):
+    soma = soma + int(input('Qual o numero da vez'))
+
+print('A soma é: ', soma)
