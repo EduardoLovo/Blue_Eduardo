9ef67f416189ba452f321b18197d159ea0d32934
EduardoLovo <eduardo.llovo@gmail.com>
2021-06-17 19:18:32 -0300

    exemplos aula 8

---
+++ Aula8/Exercicio2.py
@@ -0,0 +1,11 @@
+ 
+lista_perguntas = ["Telefonou para a vítima?",
+                       "Esteve no local do crime?",
+                       "Mora perto da vítima?",
+                       "Devia para a vítima?",
+                       "Já trabalhou com a vítima?"]
+              
+    
+for index in (lista_perguntas):
+    resposta = input(index) + "(sim ou não)."+("Resp.:")
+  
