b839013f556c3a6471d96af8190ab97f19685dba
Eduardo Lovo <eduardo.llovo@gmail.com>
2021-07-17 23:56:59 -0300

    ProjetoHumor

---
+++ Modulo_2/Projeto_humor/assets/js/index.js
@@ -0,0 +1,20 @@
+const elementoSituação = document.querySelector('#situação');
+const elementoImg =document.querySelector('#imagem');
+let elementoBtn = document.querySelector('#alterar');
+
+
+elementoBtn.addEventListener('click', () =>{
+    if(elementoBtn.value == 'primeiro'){
+        elementoImg.src = './assets/img/bebado400.png'
+        elementoSituação.innerText = 'Rick chapadão'
+        elementoBtn.value = 'segundo'
+    }else if(elementoBtn.value == 'segundo'){
+        elementoImg.src = './assets/img/toxico400.png'
+        elementoSituação.innerText = 'Rick toxico'
+        elementoBtn.value = 'terceiro'
+    }else if(elementoBtn.value == 'terceiro'){
+        elementoImg.src = './assets/img/picle400.png'
+        elementoSituação.innerText = 'PICKLE RICK!'
+        elementoBtn.value = 'primeiro'
+    }
+})
