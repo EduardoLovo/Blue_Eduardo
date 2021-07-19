089d07e9d90dc1898ee9d1cc5ad624c09117e188
Eduardo Lovo <eduardo.llovo@gmail.com>
2021-07-19 14:03:16 -0300

    Projetohumor

--- Modulo_2/Projeto_humor/assets/js/index.js
+++ Modulo_2/Projeto_humor/assets/js/index.js
@@ -1,4 +1,4 @@
-const elementoSituação = document.querySelector('#situação');
+const elementoSituacao = document.querySelector('#situacao');
 const elementoImg =document.querySelector('#imagem');
 let elementoBtn = document.querySelector('#alterar');
 
@@ -6,15 +6,25 @@ let elementoBtn = document.querySelector('#alterar');
 elementoBtn.addEventListener('click', () =>{
     if(elementoBtn.value == 'primeiro'){
         elementoImg.src = './assets/img/bebado400.png'
-        elementoSituação.innerText = 'Rick chapadão'
+        elementoSituacao.innerText = 'Rick chapadão'
         elementoBtn.value = 'segundo'
-    }else if(elementoBtn.value == 'segundo'){
+    }
+    
+    else if(elementoBtn.value == 'segundo'){
         elementoImg.src = './assets/img/toxico400.png'
-        elementoSituação.innerText = 'Rick toxico'
+        elementoSituacao.innerText = 'Rick toxico'
         elementoBtn.value = 'terceiro'
-    }else if(elementoBtn.value == 'terceiro'){
-        elementoImg.src = './assets/img/picle400.png'
-        elementoSituação.innerText = 'PICKLE RICK!'
+    }
+    
+    else if(elementoBtn.value == 'terceiro'){
+        elementoImg.src = './assets/img/ri400.png'
+        elementoSituacao.innerText = 'PICKLE RICK!'
+        elementoBtn.value = 'quarto'
+    }
+
+    else{
+        elementoImg.src = './assets/img/dançando400.png'
+        elementoSituacao.innerText = 'Rick bom de dança'
         elementoBtn.value = 'primeiro'
-    }       
+    }
 })
